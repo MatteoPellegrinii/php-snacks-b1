1f81ed204f51e38e916ab6db983c855682a334e3
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-06-27 17:53:02 +0200

    fine

---
+++ quartsnack.php
@@ -0,0 +1,26 @@
+<?php 
+    $newArray = [];
+    while (count($newArray) < 15 ) {
+        $number = rand(1, 15);
+        if (!in_array($number, $newArray)) {
+            $newArray[] = $number;
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+            for ($i=0; $i < count($newArray); $i++) { 
+                echo "<div> {$newArray[$i]} </div>";
+            };
+     ?>
+    
+</body>
+</html>
